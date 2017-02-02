4aa3c02c7eea5d243ec3d16bb9d4626266345780
Soumitra <soumitcse@gmail.com>
2017-02-02 13:15:07 +0530

    Project instalment columname changed to no_instalmanets

--- app/views/projects/_project.json.jbuilder
+++ app/views/projects/_project.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! project, :id, :name, :description, :total_budget, :from, :to, :instalments, :created_at, :updated_at
+json.extract! project, :id, :name, :description, :total_budget, :from, :to, :no_instalments, :created_at, :updated_at
 json.url project_url(project, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170202061658) do
+ActiveRecord::Schema.define(version: 20170202074033) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -85,9 +85,9 @@ ActiveRecord::Schema.define(version: 20170202061658) do
     t.integer  "total_budget"
     t.date     "from"
     t.date     "to"
-    t.integer  "instalments"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.integer  "no_instalments"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
   create_table "roles", force: :cascade do |t|
