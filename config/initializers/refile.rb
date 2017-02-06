3ef5af276316fe94d29cd9ee6044c627a4b954b6
Soumitra <soumitcse@gmail.com>
2017-02-06 11:14:05 +0530

    adding for git ignore

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -1,10 +1,10 @@
 require "refile/s3"
 
 aws = {
-  access_key_id: "AKIAJJR22GYU5XNTC7XA",
-  secret_access_key: "QmwBThG6bltC1FQtFOXPx7wnd4zrpd9kmU3hNaZ8",
+  access_key_id: "Your-access-key",
+  secret_access_key: "secreat-key",
   region: "sa-east-1",
-  bucket: "soumit-blog",
+  bucket: "bucket-name",
 }
 Refile.cache = Refile::S3.new(prefix: "cache", **aws)
 Refile.store = Refile::S3.new(prefix: "store", **aws)
