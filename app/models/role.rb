d89af3ea2d774ab4774a7bd75abff4297dba2b6a
Soumitra <soumitcse@gmail.com>
2017-02-08 13:10:44 +0530

    Association done

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,2 +1,4 @@
 class Role < ApplicationRecord
+  has_many :user_profiles
+  has_many :users_projects
 end
--- app/models/users_project.rb
+++ app/models/users_project.rb
@@ -1,4 +1,5 @@
 class UsersProject < ApplicationRecord
   belongs_to :user
   belongs_to :project
+  belongs_to :role
 end
