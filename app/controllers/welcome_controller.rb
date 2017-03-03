c8cb995841a316e9983ea1cf1573626db240eec2
Soumitra <soumitcse@gmail.com>
2017-03-03 15:57:47 +0530

    Redirect to signed in if not signed in

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,22 +4,25 @@ class WelcomeController < ApplicationController
   before_action :set_project , only: [:public_project]
 
   def index
-
-
-    @birthday_users = []
-    @user_profiles = []
-    if UserProfile.all.present?
-        @user_profiles = UserProfile.where(status: Status.where(title: "Active").first)
-        if !UserProfile.last.dob.nil?
-             @birthday_users = UserProfile.where("extract(month from dob) = ?", Date.today.month).order(:dob )
-            # UserProfile.all.each do |user_profile|
-            # end
-        end
-
+    
+    if signed_in?
+      @birthday_users = []
+      @user_profiles = []
+      if UserProfile.all.present?
+          @user_profiles = UserProfile.where(status: Status.where(title: "Active").first)
+          if !UserProfile.last.dob.nil?
+               @birthday_users = UserProfile.where("extract(month from dob) = ?", Date.today.month).order(:dob )
+              # UserProfile.all.each do |user_profile|
+              # end
+          end
+      end
+      @projects = Project.all
+      @events = Event.all
+    else
+      redirect_to new_user_session_path
     end
 
-    @projects = Project.all
-    @events = Event.all
+
   end
 
   def public_profile
