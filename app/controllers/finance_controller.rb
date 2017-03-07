72dcaf1d2f869ca1de6f6a9350da463cbd49bad5
Soumitra <soumitcse@gmail.com>
2017-03-07 15:29:06 +0530

    Saly paymnt is updated for more accuratcy

--- app/controllers/finance_controller.rb
+++ app/controllers/finance_controller.rb
@@ -12,25 +12,20 @@ class FinanceController < ApplicationController
   end
 
   def check_probation
+    doj = @user.user_profile.doj
+    probation_period = @user.salary.prov_period
+    probation_end_date = doj + probation_period.month
+    current_month_for_salary = Time.now.beginning_of_month
 
-    @transaction = SalaryTransaction.where(user_id: @user)
-    if @transaction.present?
-      if  @user.salary.prov_period <=  @transaction.count
-        respond_to do |format|
-          format.html
-        end
-      else
-        respond_to do |format|
-         format.json { render json: 'Not Yet Completed' }
-        end
-      end
-    else
+    if current_month_for_salary < probation_end_date
       respond_to do |format|
        format.json { render json: 'Not Yet Completed' }
       end
+    else
+      respond_to do |format|
+        format.html
+      end
     end
-
-
 
   end
 
@@ -46,38 +41,80 @@ class FinanceController < ApplicationController
   end
 
   def get_current_salary
-    @sal_trans = SalaryTransaction.new(user: @user)
-    # transaction = SalaryTransaction.where(user_id: @user)
+    
+    salary_month = Time.now - 1.month
+    days = Time.days_in_month(salary_month.month, salary_month.year)
 
-    if UserProfile.where(user_id: @user).first.doj < Time.now - 1.month
-      if @user.user_profile.probation
-        @sal_trans.amount = (@user.salary.initial_amount_per_month).round
-      else
-        @sal_trans.amount = (@user.salary.amount / 12).round
-      end
+    doj = @user.user_profile.doj
+
+    if  doj < Time.now - 1.month
+      working_days = days
     else
       #First Salary
-      salary_month = Time.now - 1.month
-      days = Time.days_in_month(salary_month.month, salary_month.year)
-
-      user_date_oj = UserProfile.where(user_id: @user).first.doj.day
-      working_days = days - user_date_oj
-
-      if @user.user_profile.probation
-        @sal_trans.amount = (((@user.salary.initial_amount_per_month)/days) * working_days ).round
-      else
-        @sal_trans.amount = (((@user.salary.amount / 12)/days) * working_days ).round
-      end
+       working_days = days - doj.day
     end
 
-    #Calculate Partial Salary
-    sal_month = Time.now - 1.month
-    transactionof_sal_month = SalaryTransaction.where( "extract(month from month)  = ?", sal_month.month  )
-    user_transactions_of_sal_month = transactionof_sal_month.where(user_id: @user)
-    total_transaction_for_sal_month = user_transactions_of_sal_month.sum(:amount)
+    sal_per_month = 0
 
-    #Removing Already Paid Salary for the month
-    @sal_trans.amount =   @sal_trans.amount - total_transaction_for_sal_month
+    if !@user.user_profile.probation.nil?
+
+      if  @user.user_profile.probation
+        sal = @user.salary.initial_amount_per_month
+        sal_per_month = sal
+
+        # salary_month = Time.now - 1.month
+        # days = Time.days_in_month(salary_month.month, salary_month.year)
+        sal_per_day = sal_per_month / days
+
+        #Final Slalary for Probation Period
+        salary_for_month =   (working_days * sal_per_day).round
+
+      else
+        # Final Salary for Reqular user
+        sal = @user.salary.amount
+        sal_per_month = sal / 12
+        salary_month = Time.now - 1.month
+        days = Time.days_in_month(salary_month.month, salary_month.year)
+        sal_per_day = sal_per_month / days
+
+        probation_period = @user.salary.prov_period
+        probation_end_date = doj + probation_period.month
+        probation_end_date = probation_end_date - 1.day
+        start_month = salary_month.beginning_of_month
+        end_month = salary_month.end_of_month
+
+        if probation_end_date >= start_month && probation_end_date <= end_month
+          ## Employee probation is going to over this month
+          probation_days = (probation_end_date.to_date - start_month.to_date).to_i + 1
+
+          #Probation Salary
+          prob_sal_per_day = @user.salary.initial_amount_per_month / days
+
+          probation_sal = prob_sal_per_day * probation_days
+
+          regular_days = (end_month.to_date - probation_end_date.to_date )
+          reg_sal_per_day = sal_per_day
+          reg_sal = reg_sal_per_day *  regular_days
+
+          #Final Salary for a person some for probation and remaing for regular
+          salary_for_month = (probation_sal +  reg_sal).round
+        else
+          salary_for_month =   (working_days * sal_per_day).round
+        end
+
+      end
+
+    end
+
+   #Calculate Partial Salary
+   sal_month = Time.now - 1.month
+   transactionof_sal_month = SalaryTransaction.where( "extract(month from month)  = ?", sal_month.month  )
+   user_transactions_of_sal_month = transactionof_sal_month.where(user_id: @user)
+   total_transaction_for_sal_month = user_transactions_of_sal_month.sum(:amount)
+
+   @sal_trans = SalaryTransaction.new(user: @user)
+   #Removing Already Paid Salary for the month
+   @sal_trans.amount =   salary_for_month - total_transaction_for_sal_month
 
   end
 
--- app/helpers/finance_helper.rb
+++ app/helpers/finance_helper.rb
@@ -14,27 +14,104 @@ module FinanceHelper
 
   def calculate_salary(user_id)
     working_days = get_working_days(user_id)
-
+    sal_per_month = 0
+    
     if !UserProfile.where(user_id: user_id).first.probation.nil?
 
       if  UserProfile.where(user_id: user_id).first.probation
         sal = Salary.where(user_id: user_id).first.initial_amount_per_month
         sal_per_month = sal
+        salary_month = Time.now - 1.month
+        days = Time.days_in_month(salary_month.month, salary_month.year)
+        sal_per_day = sal_per_month / days
+
+
+
+        doj = UserProfile.where(user_id: user_id).first.doj
+
+        probation_period = UserProfile.where(user_id: user_id).first.user.salary.prov_period
+        probation_end_date = doj + probation_period.month
+        probation_end_date = probation_end_date - 1.day
+        start_month = salary_month.beginning_of_month
+        end_month = salary_month.end_of_month
+
+        if probation_end_date >= start_month && probation_end_date <= end_month
+          ## Employee probation is going to over this month
+          probation_days = (probation_end_date.to_date - start_month.to_date).to_i + 1
+
+          sal = Salary.where(user_id: user_id).first.initial_amount_per_month
+          sal_per_month = sal
+          salary_month = Time.now - 1.month
+          days = Time.days_in_month(salary_month.month, salary_month.year)
+          sal_per_day = sal_per_month / days
+
+
+          probation_sal = sal_per_day * probation_days
+          regular_days = (end_month.to_date - probation_end_date.to_date )
+
+          #Calculating Regular salary per day
+          sal = Salary.where(user_id: user_id).first.amount
+          sal_per_month = sal / 12
+          salary_month = Time.now - 1.month
+          days = Time.days_in_month(salary_month.month, salary_month.year)
+          reg_sal_per_day = sal_per_month / days
+
+          reg_sal = reg_sal_per_day *  regular_days
+
+          #Final Salary for a person some for probation and remaing for regular
+          salary_for_month = (probation_sal +  reg_sal).round
+
+        else
+          #Final Slalary for Probation Period
+          salary_for_month =   (working_days * sal_per_day).round
+        end
+
       else
+        # Final Salary for Reqular user
         sal = Salary.where(user_id: user_id).first.amount
         sal_per_month = sal / 12
+        salary_month = Time.now - 1.month
+        days = Time.days_in_month(salary_month.month, salary_month.year)
+        sal_per_day = sal_per_month / days
+
+        doj = UserProfile.where(user_id: user_id).first.doj
+
+        probation_period = UserProfile.where(user_id: user_id).first.user.salary.prov_period
+        probation_end_date = doj + probation_period.month
+        probation_end_date = probation_end_date - 1.day
+        start_month = salary_month.beginning_of_month
+        end_month = salary_month.end_of_month
+
+        if probation_end_date >= start_month && probation_end_date <= end_month
+          ## Employee probation is going to over this month
+          probation_days = (probation_end_date.to_date - start_month.to_date).to_i + 1
+
+          ##Calculate Probation salary
+          prob_sal_per_month = Salary.where(user_id: user_id).first.initial_amount_per_month
+          prob_sal_per_day = prob_sal_per_month / days
+          probation_sal = prob_sal_per_day * probation_days
+          regular_days = (end_month.to_date - probation_end_date.to_date )
+
+          #Calculating Regular salary per day
+          sal = Salary.where(user_id: user_id).first.amount
+          sal_per_month = sal / 12
+          salary_month = Time.now - 1.month
+          days = Time.days_in_month(salary_month.month, salary_month.year)
+          reg_sal_per_day = sal_per_month / days
+
+          reg_sal = reg_sal_per_day *  regular_days
+
+          #Final Salary for a person some for probation and remaing for regular
+          salary_for_month = (probation_sal +  reg_sal).round
+
+        else
+          salary_for_month =   (working_days * sal_per_day).round
+        end
       end
 
     end
 
-
-
-    salary_month = Time.now - 1.month
-    days = Time.days_in_month(salary_month.month, salary_month.year)
-
-    sal_per_day = sal_per_month / days
-
-    return  (working_days * sal_per_day).round
+    return salary_for_month
 
   end
 
